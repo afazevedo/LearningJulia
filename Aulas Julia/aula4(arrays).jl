a048a95e7613a841e13fa4dbd9dea250ddd992b6
Amanda Azevedo <afazevedo29@gmail.com>
2020-07-13 09:55:41 -0300

    tutoriais

--- Aulas Julia/aula4(arrays).jl
+++ Aulas Julia/aula4(arrays).jl
@@ -15,7 +15,7 @@ Podemos chamar também um array unidimensional de lista
 a mais simples é colocar os elementos entre colchetes [ ]: =#
 
 [1, 2, 3, 4]
-['laranja', 'melancia', 'abacaxi']
+["laranja", "melancia", "abacaxi"]
 
 #=
 O primeiro exemplo é uma lista de quatro números inteiros.
@@ -25,7 +25,7 @@ Porém, os elementos de uma lista não precisam ser do mesmo tipo.
 A lista seguinte contém uma string, um número de ponto flutuante,
 um número inteiro e também, outra lista:
 =#
-['spam', 2.3, 5, [1, 2]]
+["spam", 2.3, 5, [1, 2]]
 
 #Chamaremos uma lista dentro de outra lista de lista aninhada.
 #Chamaremos também uma lista que não contém elementos lista vazia;
